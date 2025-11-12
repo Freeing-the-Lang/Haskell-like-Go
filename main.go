fd893881e3c4bb5d0c5d2c4e0fbbd2faa29cf85b
한진희 <0200134hjh@gmail.com>
2025-11-13 04:19:33 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Functional helpers
+func Map[T any, R any](xs []T, f func(T) R) []R {
+	out := make([]R, len(xs))
+	for i, v := range xs {
+		out[i] = f(v)
+	}
+	return out
+}
+
+func Filter[T any](xs []T, f func(T) bool) []T {
+	out := []T{}
+	for _, v := range xs {
+		if f(v) {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+func Foldl[T any, R any](xs []T, init R, f func(R, T) R) R {
+	acc := init
+	for _, v := range xs {
+		acc = f(acc, v)
+	}
+	return acc
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+
+	squares := Map(nums, func(x int) int { return x * x })
+	filtered := Filter(squares, func(x int) bool { return x > 5 })
+	sum := Foldl(filtered, 0, func(acc, x int) int { return acc + x })
+
+	fmt.Println("squares:", squares)
+	fmt.Println("filtered:", filtered)
+	fmt.Println("sum:", sum)
+}
